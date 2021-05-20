5bc944aff479f1795bf675c315d9e795180c13b5
gmaheshwari2006 <>
2021-05-20 21:58:01 +0000

    added Node class

--- linkedList/CustomLinkedList.js
+++ linkedList/CustomLinkedList.js
@@ -1,9 +1,7 @@
 class Node {
-  constructor(value) {
-    this.node = {
-      value: value,
-      next: null
-    }
+  constructor(value) {   
+      this.value = value;
+      this.next = null;    
   }
 }
 
@@ -19,6 +17,10 @@ class customLinkedList {
 
   append(value) {
     const node = new Node(value);
+    /*const node = {
+      value: value,
+      next: null
+    }*/
     this.tail.next = node;
     this.tail = node;
     this.lenght++;
@@ -26,9 +28,27 @@ class customLinkedList {
 
   prepend(value) {
     const node = new Node(value);
+    /*const node = {
+      value: value,
+      next: null
+    }*/
     node.next = this.head;
     this.head = node;
     this.lenght++;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode != null) {
+      console.log('currentNode', currentNode);
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log('linked list is: ', array);
+  }
+  insert(index, value) {
+    
   }
 }
 
@@ -36,6 +56,8 @@ const myLinkedList = new customLinkedList(10);
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
-console.log(myLinkedList);
+//myLinkedList.insert(2, 99);
+myLinkedList.printList();
+//console.log(myLinkedList);
 
-//10 5 16
+//1 10 5 16 
